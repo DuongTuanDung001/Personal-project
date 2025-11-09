069836ff394de5c915356aad1b2466ae7aebd237
DuongTuanDung001 <duongtuandung123123@gmail.com>
2025-11-09 19:10:23 +0700

    Add files via upload

---
+++ Project_do_nuoc/Project_do_nuoc.ino
@@ -0,0 +1,54 @@
+#include <WiFi.h>
+#include "secrets.h"
+#include "ThingSpeak.h" // always include thingspeak header file after other header files and custom macros
+
+char ssid[] = SECRET_SSID;   // your network SSID (name) 
+char pass[] = SECRET_PASS;   // your network password
+int keyIndex = 0;            // your network key Index number (needed only for WEP)
+WiFiClient  client;
+
+unsigned long myChannelNumber = SECRET_CH_ID;
+const char * myWriteAPIKey = SECRET_WRITE_APIKEY;
+
+
+void setup() {
+  Serial.begin(115200);  //Initialize serial
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for Leonardo native USB port only
+  }
+  pinMode(34, INPUT);
+  
+  WiFi.mode(WIFI_STA);   
+  ThingSpeak.begin(client);  // Initialize ThingSpeak
+}
+
+void loop() {
+
+  // Connect or reconnect to WiFi
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(SECRET_SSID);
+    while(WiFi.status() != WL_CONNECTED){
+      WiFi.begin(ssid, pass);  // Connect to WPA/WPA2 network. Change this line if using open or WEP network
+      Serial.print(".");
+      delay(5000);     
+    } 
+    Serial.println("\nConnected.");
+  }
+  int value = analogRead(34);
+  // set the fields with the values
+  ThingSpeak.setField(1, int((4096-value)/40.96) );
+
+  Serial.println(value);
+  // write to the ThingSpeak channel
+  int x = ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+  if(x == 200){
+    Serial.println("Channel update successful.");
+  }
+  else{
+    Serial.println("Problem updating channel. HTTP error code " + String(x));
+  }
+  
+  
+  delay(20000); // Wait 20 seconds to update the channel again
+}
---
+++ Project_do_nuoc/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "BKSTAR_T04"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "stemstar"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 3154440			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "OG7Z7WSWXEX7XVUW"   // replace XYZ with your channel write API Key
